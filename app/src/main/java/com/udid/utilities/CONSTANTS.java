8fcb1b2ae006ea78ab4e143e2010c9c72d5b4da8
Akshat_Bansal_11 <akshat.11dec@gmail.com>
2025-01-27 12:06:08 +0530

    url change

--- app/src/main/java/com/udid/utilities/CONSTANTS.java
+++ app/src/main/java/com/udid/utilities/CONSTANTS.java
@@ -3,5 +3,5 @@ package com.udid.utilities;
 public class CONSTANTS {
 
     public static boolean isDebug = true;
-    public static String BASE_URL = "http://134.209.222.136:86/api/rest/";
+    public static String BASE_URL = "http://134.209.222.136:86/api/rest2/";
 }
