a63ac86b9ed439089715ecc5aa8a1757b160998a
Shreyash <shreyashjain33@gmail.com>
2025-02-21 12:38:03 +0530

    pull

--- app/src/main/java/com/swavlambancard/udid/viewModel/SharedDataViewModel.kt
+++ app/src/main/java/com/swavlambancard/udid/viewModel/SharedDataViewModel.kt
@@ -7,7 +7,7 @@ import com.swavlambancard.udid.model.PwdApplication
 
 
 class SharedDataViewModel : ViewModel() {
-    val userData = MutableLiveData(PwdApplication()) // Initialize with default fields
+    val userData = MutableLiveData(PwdApplication()) // Initialize with default fields// toh prevent data from being affected by recreational changes
 
     init {
         userData.value = PwdApplication() // Initialize with default empty fields
